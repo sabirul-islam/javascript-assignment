85c76556d2b1aa553f9ec9fb94538afb207fd73a
sabirul-islam <sabirul.shimul@gmail.com>
2020-07-17 11:14:49 +0600

    final

--- assignment.js
+++ assignment.js
@@ -16,12 +16,12 @@ function feetToMile(feet){
     var tableCount = 3 * table;
     var BedCount = 5 * bed;
     var total = chairCount + tableCount + BedCount;
-    return total + " cft";
+    return total;
     }
 
 
 //Finding Tiny Friend
-function tinyFriend(friends) {
+function tinyFriend(friends){
     var tiny = friends [0];
 
     for (var i = 0; i < friends.length; i++){
@@ -32,4 +32,18 @@ function tinyFriend(friends) {
         }
         }
     return tiny;
+    }
+
+// Brick calculation
+function brickCalculator(storeyed){
+    if(storeyed<=10){
+        return storeyed * 15 * 1000;
+    }
+    if (storeyed<=20){
+        return (storeyed-10) * 12 * 1000 + 150000;
+    }
+    if (storeyed<=Infinity){
+        return (storeyed-20) * 10 * 1000 + 270000;
+    }
+    return storeyed;
     }
